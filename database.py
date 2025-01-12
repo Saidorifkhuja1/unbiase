327e306eef39a79f53fcf906275de17c68e9eb68
Saidorifkhuja Yuldoshkhujaev <yoldoshxojayevsaidorifxoja@gmail.com>
2025-01-12 20:56:58 +0500

    asg

--- database.py
+++ database.py
@@ -1,16 +1,14 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.pool import NullPool
-
+from config import settings
 class Base(DeclarativeBase):
     pass
 
-DATABASE_URL = "postgresql+asyncpg://postgres:start2024@localhost:5434/unibase"
 
-# DATABASE_URL = 'sqlite+aiosqlite:///base.db'
 
 engine = create_async_engine(
-    DATABASE_URL,
+    settings.DATABASE_URL_asycpg,
     echo=True,
     poolclass=NullPool
 )
